853a18e545bcb53062c0954c2cdcfb86b2bd30be
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-12-09 02:13:24 -0300

    Seção 2 - Introdução à testes com o Jest e Testing Library - Finalizado

--- secao2/src/components/Button/index.jsx
+++ secao2/src/components/Button/index.jsx
@@ -1,15 +1,15 @@
 import { Component } from 'react';
 import './styles.css';
 
-export const Button = ({onClick, disabled, text}) => {
+export const Button = ({ text, onClick, disabled }) => {
   return (
-      <button
-          className='button'
-          onClick={onClick}
-          disabled={disabled}
-      >
-        {text}
-      </button>
+    <button
+      className='button'
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {text}
+    </button>
   );
 }
 
