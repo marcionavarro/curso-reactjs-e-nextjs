383a1c161ebdf7d2d59bcc2d9272b256a32013d3
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-12-29 17:54:46 -0300

    Seção 4 - Organização com useEffect, useReducer, useContext e useRef - Part 2

--- secao4/src/templates/App/index.jsx
+++ secao4/src/templates/App/index.jsx
@@ -1,14 +1,17 @@
 import './style.css';
 import { PostsProvider } from '../../contexts/PostsProvider';
 import { Posts } from '../../components/posts';
+import { CounterProvider } from '../../contexts/CounterProvider';
 
 function App() {
   return (
-    <PostsProvider>
-      <div>
-        <Posts />
-      </div>
-    </PostsProvider>
+    <CounterProvider>
+      <PostsProvider>
+        <div>
+          <Posts />
+        </div>
+      </PostsProvider>
+    </CounterProvider>
   );
 }
 
