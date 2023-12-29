1b75ae3fec1127aaeb631caeae5efe08f710c421
marcionavarro <marcionavarrodearaujo@gmail.com>
2023-12-29 15:55:28 -0300

    Seção 4 - MyHook

--- secao4/src/App.js
+++ secao4/src/App.js
@@ -1,72 +1,46 @@
-import P from 'prop-types';
-import { createContext, useContext, useReducer, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
+// refer: https://overreacted.io/making-setinterval-declarative-with-react-hooks/
 
-// actions.js
-export const actions = {
-  CHANGE_TITLE: 'CHANGE_TITLE',
-};
+const useMyHook = (cb, delay = 1000) => {
+  const savedCb = useRef();
 
-// data.js
-const globalState = {
-  title: 'O Título do reducer',
-  body: 'O paragrafo',
-  counter: 0,
-};
+  useEffect(() => {
+    savedCb.current = cb;
+  }, [cb]);
 
-// reducer.js
-export const reducer = (state, action) => {
-  switch (action.type) {
-    case actions.CHANGE_TITLE: {
-      console.log('MUDAR TITULO');
-      return { ...state, title: actions.payload };
-    }
-  }
-  return { ...state };
-};
+  useEffect(() => {
+    const interval = setInterval(() => {
+      savedCb.current();
+    }, delay);
 
-// AppContext
-export const Context = createContext();
-export const AppContext = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, globalState);
-
-  const changeTitle = (payload) => {
-    dispatch({ type: actions.CHANGE_TITLE, payload });
-  };
-
-  return (
-    <Context.Provider value={{ state, changeTitle }}>
-      {children}
-    </Context.Provider>
-  );
-};
-
-AppContext.propTypes = {
-  children: P.node,
-};
-
-// H1/index.jsx
-export const H1 = () => {
-  const context = useContext(Context);
-  const inputRef = useRef();
-
-  return (
-    <>
-      <h1 onClick={() => context.changeTitle(inputRef.current.value)}>
-        {context.state.title}
-      </h1>
-      <input type="text" ref={inputRef} />
-    </>
-  );
+    return () => clearInterval(interval);
+  }, [delay]);
 };
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [delay, setDelay] = useState(1000);
+  const [incrementor, setIncrementor] = useState(100);
+
+  useMyHook(() => setCounter((c) => c + 1), delay);
+
   return (
-    <AppContext>
-      <div>
-        <H1 />
-      </div>
-    </AppContext>
+    <div>
+      <h1>Contator: {counter}</h1>
+      <h1>Delay: {delay}</h1>
+      <button onClick={() => setDelay((d) => d + incrementor)}>
+        +{incrementor}
+      </button>
+      <button onClick={() => setDelay((d) => d - incrementor)}>
+        -{incrementor}
+      </button>
+      <input
+        type="number"
+        value={incrementor}
+        onChange={(e) => setIncrementor(Number(e.target.value))}
+      />
+    </div>
   );
 }
 
