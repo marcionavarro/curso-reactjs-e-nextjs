5feb99815ce1036ca4c03f2a1d7a19ef14d2fd17
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-09-12 02:59:15 -0300

    Seção 4 - useAsync - Terceiro custom hook

--- secao4/src/templates/App/index.jsx
+++ secao4/src/templates/App/index.jsx
@@ -1,19 +1,11 @@
 import './style.css';
-import { PostsProvider } from '../../contexts/PostsProvider';
-import { Posts } from '../../components/posts';
-import { CounterProvider } from '../../contexts/CounterProvider';
 import { Home } from './Home';
 
 function App() {
   return (
-    <CounterProvider>
-      <PostsProvider>
-        <div>
-          <Home />
-          {/* <Posts /> */}
-        </div>
-      </PostsProvider>
-    </CounterProvider>
+    <div>
+      <Home />
+    </div>
   );
 }
 
