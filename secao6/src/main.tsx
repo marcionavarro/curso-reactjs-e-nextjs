7e59ae304ff5621d5151a47e2e34991ef8d34ede
marcionavarro <marcionavarrodearaujo@gmail.com>
2024-09-14 15:24:16 -0300

    Seção 6 - Instalando o React Router Dom v6 e configurando o projeto, Criando as primeiras rotas com BrowserRouter, Routes e Route, Usando Links: Link vs a

--- secao6/src/main.tsx
+++ secao6/src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Home } from './components/Home'
+import { About } from './components/About'
+import './styles/global.css'
+import { Menu } from './components/Menu'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Menu />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
