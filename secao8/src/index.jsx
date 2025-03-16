d920b741b13e87210a7cee8b23fabcfe740bc118
marcionavarro <marcionavarrodearaujo@gmail.com>
2025-03-16 02:16:05 -0300

    Seção 8 - Configurando o Styled-Components, Configurando o ThemProvider do styled-components

--- secao8/src/index.jsx
+++ secao8/src/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Home from './templates/App';
+import { GlobalStyles } from './styles/gobal-styles';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './styles/theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <ThemeProvider theme={theme}>
+      <Home />
+      <GlobalStyles />
+    </ThemeProvider>
   </React.StrictMode>
 );
